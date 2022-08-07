c13090aff2b7f17d145449fbfc891fe4401306ae
Vaishakh <54908817+Vaishakh-SM@users.noreply.github.com>
2022-08-07 10:13:53 +0530

    Revert "Vaishakh2"

--- app/main.py
+++ app/main.py
@@ -1,22 +1,24 @@
-from os import access
-from fastapi import FastAPI, Header, status, Request
-from fastapi.exceptions import HTTPException
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, FastAPI
+from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 from .config import settings
 import requests
-# from sqlalchemy.orm import Session
-# from . import models
-# from .database import SessionLocal, engine
+from sqlalchemy.orm import Session
+from . import models
+from .database import SessionLocal, engine
 from starlette.middleware.cors import CORSMiddleware
 
-# models.Base.metadata.create_all(bind=engine)
+
+import jwt
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 origins = [
     "http://localhost",
     "http://localhost:3000",
+    "http://localhost:3001"
 ]
 
 app.add_middleware(
@@ -27,77 +29,74 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 # Dependency
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
+app = FastAPI()
 
+# print(settings.client_id)
 class Req(BaseModel):
     code: str
 
+# sample_response = {
+#   "id_token":"ID_TOKEN_VALUE",
+#   "access_token":"ACCESS_TOKEN_VALUE",
+#   "expires_in":3600,
+#   "token_type":"Bearer"
+# }
+  
 
-@app.post("/api/authorize")
-async def authorize_mediavalet(code: Req):
-
+@app.post("/")
+async def authorize_mediavalet(code: str, db: Session = Depends(get_db)):
     url = "https://login.mediavalet.com/connect/token"
 
-    response = requests.post(
-        url,
-        data={
-            "grant_type": "authorization_code",
-            "code": code.code,
-            "redirect_uri": settings.frontend_url,
-            "client_id": settings.client_id,
-            "client_secret": settings.client_secret,
-        },
-        headers={'Content-Type': 'application/x-www-form-urlencoded'})
-    
+    response = requests.post(url, data={
+        "grant_type":"authorization_code",
+        "code":code,
+        "redirect_uri":settings.backend_url,
+        "client_id":settings.client_id,
+        "client_secret":settings.client_secret,
+    }, headers={
+        'Content-Type': 'application/x-www-form-urlencoded'
+    })
     result = response.json()
-    print(result)
+    auth_token = result["access_token"]
+    print(auth_token)
 
     return result
 
+    # url2 = "https://api.mediavalet.com/users/current"
+    # response2 = requests.get(url2, headers={
+    #     'Content-Type': 'application/x-www-form-urlencoded',
+    #     "Ocp-Apim-Subscription-Key": settings.primary_key,
+    #     "Authorization": "Bearer " + auth_token,
+    # })
+    # result2 = response2.json()
+    # id = result2["payload"]["id"]
 
-@app.get("/categories")
-async def get_categories(req: Request):
-    url = "https://api.mediavalet.com/categories"
+    # db_user = models.User(id=id, auth_token=auth_token)
+    # user = db.get(models.User, id)
+    # if not user:
+    #     db.add(db_user)
+    #     db.commit()
+    #     db.refresh(db_user)
+    # else:
+    #     setattr(user, "auth_token", auth_token)
+    #     db.commit()
+    #     db.refresh(user)
 
-    header = req.headers["Authorization"].split()
-    access_token = ""
-    if header[0] == "Bearer":
-        access_token = header[1]
-    else:
-        raise HTTPException(status_code=401, detail="Unauthorized")
-    response = requests.get(url,
-                            headers={
-                                'Content-Type':
-                                'application/x-www-form-urlencoded',
-                                "Ocp-Apim-Subscription-Key":
-                                settings.primary_key,
-                                "Authorization": "Bearer " + access_token,
-                            })
+    # encoded_jwt = jwt.encode({"id": id}, "secret", algorithm="HS256")
+    # return RedirectResponse(settings.frontend_url + "/redirect/" + encoded_jwt.decode("UTF-8"))
+
+@app.get("/folders")
+async def get_folders():
+    url = "https://api.mediavalet.com/folders"
     
-    result = response.json()
-    categories = []
-    for res in result["payload"]:
-        category = {
-            "id": res["id"],
-            "name": res["tree"]["name"]
-        }
-        categories.append(category)
-
-    return categories
-
-
-@app.get("/user")
-async def get_user():
-    url = "https://api.mediavalet.com/users/current"
-
     try:
         f = open("media_token.txt", "r")
         auth_token = f.read()
@@ -105,15 +104,36 @@ async def get_user():
     except FileNotFoundError:
         return {"error": "Auth token not found"}
 
-    response = requests.get(url,
-                            headers={
-                                'Content-Type':
-                                'application/x-www-form-urlencoded',
-                                "Ocp-Apim-Subscription-Key":
-                                settings.primary_key,
-                                "Authorization": "Bearer " + auth_token,
-                            })
+    response = requests.get(url, headers={
+        'Content-Type': 'application/x-www-form-urlencoded',
+        "Ocp-Apim-Subscription-Key": settings.primary_key,
+        "Authorization": "Bearer " + auth_token,
+    })
+    result = response.json()
+    print(result)
+
+    return result
+
+@app.get("/user")
+async def get_user():
+    url = "https://api.mediavalet.com/users/current"
+    
+    try:
+        f = open("media_token.txt", "r")
+        auth_token = f.read()
+        f.close()
+    except FileNotFoundError:
+        return {"error": "Auth token not found"}
+
+    response = requests.get(url, headers={
+        'Content-Type': 'application/x-www-form-urlencoded',
+        "Ocp-Apim-Subscription-Key": settings.primary_key,
+        "Authorization": "Bearer " + auth_token,
+    })
     result = response.json()
     print(result["payload"]["id"])
 
     return result
+
+
+
